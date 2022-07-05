b10d33dbbdad43462d00ffd7f7524d80b5ed91ac
abharms <abharms@gmail.com>
2022-07-05 10:45:53 -0500

    adjust mobile hero text

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -24,9 +24,9 @@ function Hero() {
         <div id="hero" ref={parallax.ref} className="relative grid grid-cols-6 gap-4 place-content-evenly hero-bg flex relative z-10">
             <Navbar />
             <div className='col-start-2 col-span-4 text-center'>
-                <h1 className='mt-28 md:mt-0 text-5xl md:text-7xl text-white'>We provide personal service for your insurance needs</h1>
+                <h1 className='mt-32 md:mt-0 text-5xl md:text-7xl text-white'>We provide personal service for your insurance needs</h1>
 
-                <div className='mt-14'>
+                <div className='mt-8 md:mt-14'>
                     <button onClick={() => onAboutUsClick()} className="btn btn-lg btn-primary btn-wide md:mr-5">About Us</button>
                     <button onClick={() => onContactClick()} className="btn btn-lg btn-wide btn-outline text-white mt-5">Contact</button>
                 </div>
